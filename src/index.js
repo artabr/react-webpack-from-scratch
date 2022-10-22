8364437fb6dfc71792dc218b4ed904214b1d8133
Artem Abramov <Artem_Abramov1@epam.com>
2022-10-22 12:58:52 +0300

    Configured ESlint and Prettier

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import App from './App';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.createRoot(document.getElementById('app')).render(<App />);
